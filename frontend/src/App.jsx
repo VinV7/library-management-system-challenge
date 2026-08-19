81bbd0bdf94eb6721b6321219674c28f0c50acb5
Albin von Ivandito-Versternberg <alvin7ff@gmail.com>
2026-08-19 21:51:10 +0800

    feat: add navbar and hero skeleton

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
+// Main Libraries Import 
 import { Routes, Route } from 'react-router-dom'
+
+// File Import 
+import Homepage from './pages/Homepage'
 
 function App() {
   return (
     <Routes>
-      <Route path='/'></Route>
+      <Route path='/' element={<Homepage/>}></Route>
     </Routes>
   )
 }
---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,14 @@
+function Homepage() {
+    return (
+        <div className="flex flex-col h-screen">
+            <nav className="w-screen h-20 bg-taupe-800">
+
+            </nav>
+            <div className="flex-1 w-screen bg-yellow-50">
+
+            </div>
+        </div>
+    )
+}
+
+export default Homepage
