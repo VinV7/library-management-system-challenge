4dc366a9bc25d898042d9aa299afa5ed17f75c27
Albin von Ivandito-Versternberg <alvin7ff@gmail.com>
2026-08-21 05:20:29 +0800

    feat: registered Login.jsx as /login route in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,14 @@ import { Routes, Route } from 'react-router-dom'
 // File Import 
 import Homepage from './pages/Homepage'
 import Register from './pages/Register'
+import Login from './pages/Login'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Homepage/>}></Route>
       <Route path='/register' element={<Register/>}></Route>
+      <Route path='/login' element={<Login/>}></Route>
     </Routes>
   )
 }
