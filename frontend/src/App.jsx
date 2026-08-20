bb3944c546e0d49b1eba37ae4fe5e77c487ad6d4
Albin von Ivandito-Versternberg <alvin7ff@gmail.com>
2026-08-21 04:32:54 +0800

    feat: make register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,13 @@ import { Routes, Route } from 'react-router-dom'
 
 // File Import 
 import Homepage from './pages/Homepage'
+import Register from './pages/Register'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Homepage/>}></Route>
+      <Route path='/register' element={<Register/>}></Route>
     </Routes>
   )
 }
