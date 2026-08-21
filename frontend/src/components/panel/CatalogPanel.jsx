84ff61304ecfa9d2545bd9d3b3a05b9aedd79845
Albin von Ivandito-Versternberg <alvin7ff@gmail.com>
2026-08-21 21:44:18 +0800

    feat: initialize initial CatalogPanel HTML skeleton structure

---
+++ frontend/src/components/panel/CatalogPanel.jsx
@@ -0,0 +1,14 @@
+function CatalogPanel() {
+    return (
+        <div className="flex flex-1 min-h-screen bg-yellow-50">
+            <div className="flex-1 px-12 py-10 bg-yellow-50">
+                
+            </div> 
+            <div className="w-96 bg-white">
+                
+            </div>
+        </div>
+    )
+}
+
+export default CatalogPanel
--- frontend/src/pages/MasterLibrary.jsx
+++ frontend/src/pages/MasterLibrary.jsx
@@ -1,24 +1,30 @@
-// Main Import 
+// Main Import
 import { useState } from "react";
 
 // Components Import
 import Sidebar from "../components/Sidebar";
+import CatalogPanel from "../components/panel/CatalogPanel";
 
 function MasterLibrary() {
-    const [activePanel, setActivePanel] = useState('Catalog')
+  const [activePanel, setActivePanel] = useState("Catalog");
 
   return (
     <div className="flex">
-      <Sidebar
-        buttons={[
-          { key: "Catalog" },
-          { key: "Add Book" },
-          { key: "Manage Member" },
-          { key: "Manage Librarian" },
-        ]} 
-        activePanel={activePanel}
-        setActivePanel={setActivePanel}
-      ></Sidebar>
+      <div>
+        <Sidebar
+          buttons={[
+            { key: "Catalog" },
+            { key: "Add Book" },
+            { key: "Manage Member" },
+            { key: "Manage Librarian" },
+          ]}
+          activePanel={activePanel}
+          setActivePanel={setActivePanel}
+        ></Sidebar>
+      </div>
+      <div className={activePanel === "Catalog" ? "flex flex-1 flex-col min-h-0" : "hidden"}>
+        <CatalogPanel></CatalogPanel>
+      </div>
     </div>
   );
 }
