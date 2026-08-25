7900b51ad749ee44e7a8eed3a9ac7d4a7a1d4f75
Albin von Ivandito-Versternberg <alvin7ff@gmail.com>
2026-08-25 14:08:06 +0800

    feat: finish html skeletal structure for Member.jsx

---
+++ frontend/src/components/panels/MemberCatalogPanel.jsx
@@ -0,0 +1,9 @@
+function MemberCatalogPanel() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default MemberCatalogPanel
--- frontend/src/pages/Member.jsx
+++ frontend/src/pages/Member.jsx
@@ -1,9 +1,35 @@
+// Main Import
+import { useState } from "react";
+
+// Components Import
+import Sidebar from "../components/Sidebar";
+import MemberCatalogPanel from "../components/panels/MemberCatalogPanel";
+
 function Member() {
-    return (
-        <div>
-            Hello World
-        </div>
-    )
+  const [activePanel, setActivePanel] = useState("Catalog");
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <Sidebar
+        buttons={[{ key: "Catalog" }, { key: "Borrowed Books" }]}
+        activePanel={activePanel}
+        setActivePanel={setActivePanel}
+      ></Sidebar>
+      <div
+        className={
+          activePanel === "Catalog" ? "flex flex-1 flex-col min-h-0" : "hidden"
+        }
+      >
+        <MemberCatalogPanel></MemberCatalogPanel>
+      </div>
+      <div
+        className={
+          activePanel === "Catalog" ? "flex flex-1 flex-col min-h-0" : "hidden"
+        }
+      >
+        <MemberCatalogPanel></MemberCatalogPanel>
+      </div>
+    </div>
+  );
 }
 
-export default Member
+export default Member;
