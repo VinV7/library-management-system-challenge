84a4522676876d37962ea3dafd3f6c87968d3986
Albin von Ivandito-Versternberg <alvin7ff@gmail.com>
2026-08-26 19:19:19 +0800

    feat: make session cookies

---
+++ backend/app/core/Session.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Core;
+
+class Session
+{
+    public static function set(string $sessionId): void
+    {
+        setcookie('session_id', $sessionId, [
+            'expires' => time() + 86400,
+            'path' => '/',
+            'httponly' => true,
+            'secure' => true,
+            'samesite' => 'Lax'
+        ]);
+    }
+
+    public static function get(): ?string
+    {
+        return $_COOKIE['session_id'] ?? null;
+    }
+
+    public static function destroy(): void
+    {
+        setcookie('session_id', '', [
+            'expires' => time() - 3600,
+            'path' => '/',
+            'httponly' => true,
+            'secure' => true,
+            'samesite' => 'Lax'
+        ]);
+    }
+}
