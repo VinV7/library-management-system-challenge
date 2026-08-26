c15e942239d08d91c7f1dd2b83b4578665933e7f
Albin von Ivandito-Versternberg <alvin7ff@gmail.com>
2026-08-26 18:19:50 +0800

    feat: make registry model queries

---
+++ backend/app/models/Registry.php
@@ -0,0 +1,53 @@
+<?php 
+
+namespace App\Models;
+
+use App\Core\Database;
+use PDO;
+
+class Registry
+{
+    public static function checkUser(string $email): array
+    {
+        $db = Database::connection();
+
+        $sql = "SELECT FROM users WHERE email = :email";
+
+        $stmt = $db->prepare($sql);
+
+        $stmt->execute([
+            'email' => $email
+        ]);
+
+        return $stmt->fetch();
+    }
+
+    public static function registerUser(string $email, string $username, string $password): void
+    {
+        $db = Database::connection();
+
+        $sql = "
+            INSERT INTO users (
+                email,
+                username,
+                password,
+                role
+            )
+            VALUES (
+                :email,
+                :username,
+                :password,
+                :role
+            )
+        ";
+
+        $stmt = $db->prepare($sql);
+
+        $stmt->execute([
+            "email" => $email,
+            "username" => $username,
+            "password" => $password,
+            "role" => "Member"
+        ]);
+    }
+}
